f8d19315f159415068fada7eff00764d8dc6dd0c
vimuw <vimuwijesinghe@gmail.com>
2026-03-07 16:12:47 +0100

    Remove unused legacy files

--- modelli.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-# Ricordati di incollare la tua vera API KEY qui
-client = genai.Client(api_key="AIzaSyD3wjz8-vCJREY-u4X4LKEH1J-SHwScgag")
-
-print("Ecco TUTTI i modelli disponibili per la tua chiave:")
-for m in client.models.list():
-    print(m.name)
